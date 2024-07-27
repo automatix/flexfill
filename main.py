9196cec1764f34a8508f5035ec425088554da6a8
automatix <khanataev@gmail.com>
2024-07-27 17:12:43 +0200

    The mapping made more generic. A generic mapping functio implemented. It accepts as input the following:
    - the XPath to the according form element
    - data array of field names (the JSON path to the field in th einput JSON file) or just one name, if there is only one
    - the strategy as anonymous function, that transforms the data from the relevant input JSON fields to the form field

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import json
 import yaml
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+
 from mapping.dummy import fill_form
 
 with open('configs.yaml', 'r') as f:
@@ -13,7 +15,10 @@ chrome_service = Service(config['chrome_driver_path'])
 driver = webdriver.Chrome(service=chrome_service)
 driver.get(config['form_url'])
 
-fill_form(driver, data)
+fill_form(driver, data, element_xpath='//input[@id="edit-name"]', data_fields='name')
+def custom_strategy(driver, data):
+    driver.find_element(By.XPATH, '//input[@id="edit-email"]').send_keys(data.get('email', ''))
+fill_form(driver, data, strategy=custom_strategy)
 
 try:
     while True:
--- mapping/dummy.py
+++ mapping/dummy.py
@@ -1,4 +1,18 @@
 from selenium.webdriver.common.by import By
 
-def fill_form(driver, data):
-    driver.find_element(By.ID, 'edit-name').send_keys(data['name'])
+def fill_form(driver, data, element_xpath=None, data_fields=None, strategy=None):
+    if strategy:
+        # Use the provided strategy to transform data and fill the form
+        strategy(driver, data)
+    elif element_xpath and data_fields:
+        if isinstance(data_fields, list):
+            # Handle multiple fields
+            for field in data_fields:
+                element = driver.find_element(By.XPATH, element_xpath)
+                element.send_keys(data.get(field, ''))
+        else:
+            # Handle single field
+            element = driver.find_element(By.XPATH, element_xpath)
+            element.send_keys(data.get(data_fields, ''))
+    else:
+        raise ValueError("Either element_xpath and data_fields or strategy must be provided.")
